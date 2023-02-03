466a32a41d62e15effa6f5f766c07c67c597fbdb
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-02-03 19:08:22 -0300

    Atulização

---
+++ delivery_app/lib/app/core/ui/styles/colors_app.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static ColorsApp? _instance;
+
+  ColorsApp._();
+
+  static ColorsApp get i {
+    _instance ??= ColorsApp._();
+    return _instance!;
+  }
+
+  Color get primary => const Color(0XFF007D21);
+  Color get secudary => const Color(0XFFF88B0C);
+}
+
+extension ColorsAppExcetions on BuildContext {
+  ColorsApp get colors => ColorsApp.i;
+}
---
+++ delivery_app/lib/app/dw9_delivery_app.dart
@@ -0,0 +1,17 @@
+import 'package:delivery_app/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+
+  const Dw9DeliveryApp({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return MaterialApp(
+        title: 'Delivery App',
+        routes: {
+          '/':(context) => const SplashPage(),
+        },
+       );
+  }
+}
